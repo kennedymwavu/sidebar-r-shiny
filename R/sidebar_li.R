a664a09e5d3a00e24dc6d0ea09561cf6d1a2bd41
kennedymwavu <mwavukennedy@gmail.com>
2023-05-27 02:26:51 +0300

    ft: add sidebar_li()

---
+++ R/sidebar_li.R
@@ -0,0 +1,50 @@
+#' Create a sidebar list item
+#'
+#' This function creates a list item for a sidebar menu
+#'
+#' @param item_class The class of the list item. Default is "nav-item".
+#' @param href The URL for the link. Default is "#".
+#' @param link_class The class of the link. Default is "nav-link text-white".
+#' @param aria_current The value of the "aria-current" attribute.
+#' Default is "false".
+#' @param icon_name The name of the icon. Default is NULL.
+#' @param icon_class The class of the icon. Default is NULL.
+#' @param label The label for the link.
+#'
+#' @return A `shiny::tags$li` object representing the sidebar list item.
+#'
+#' @examples
+#' sidebar_li(
+#'   item_class = "nav-item",
+#'   href = "#",
+#'   link_class = "nav-link text-white",
+#'   aria_current = "false",
+#'   icon_name = NULL,
+#'   icon_class = "bi bi-grid pe-none me-2", # bootstrap icon
+#'   label = " Products "
+#' )
+#'
+#' @export
+sidebar_li <- function(
+    item_class = "nav-item",
+    href = "#",
+    link_class = "nav-link text-white",
+    aria_current = "false",
+    icon_name = NULL,
+    icon_class = NULL,
+    label = NULL
+) {
+  shiny::tags$li(
+    class = item_class,
+    tags$a(
+      href = href,
+      class = link_class,
+      "aria-current" = aria_current,
+      shiny::icon(
+        name = NULL,
+        class = icon_class
+      ),
+      label
+    )
+  )
+}
