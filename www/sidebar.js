4a5669645032fa3b70b0dc5ab750ca76377a1ddb
kennedymwavu <mwavukennedy@gmail.com>
2023-05-27 14:56:09 +0300

    ch: refactor `make_active()`

--- www/sidebar.js
+++ www/sidebar.js
@@ -10,14 +10,21 @@
 let sidebar_ul = document.querySelector("#sidebar_ul");
 let sidebar_li = sidebar_ul.querySelectorAll("li");
 sidebar_li.forEach((x) => {
-  x.addEventListener("click", () => {
-    // remove class 'active' from every other li:
-    let anchor_elements = sidebar_ul.querySelectorAll("a");
-    anchor_elements.forEach((a) => {
-      a.classList.remove("active");
-    });
-    
-    // add class 'active' to this element:
-    x.querySelector("a").classList.add("active");
-  });
+  x.addEventListener("click", () => make_active(x));
 });
+
+let make_active = function (x) {
+  // remove class 'active' and set 'aria-current' to 'false' for all sidebar
+  // anchor elements:
+  let anchor_elements = sidebar_ul.querySelectorAll("a");
+  anchor_elements.forEach((a) => {
+    a.classList.remove("active");
+    a.setAttribute("aria-current", "false");
+  });
+
+  // add class 'active' and set 'aria-current' to 'page' for the clicked
+  // sidebar anchor element:
+  let a = x.querySelector("a");
+  a.classList.add("active");
+  a.setAttribute("aria-current", "page");
+};
