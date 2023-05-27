76b84a647ec4dd7e7ebb8d426025a17511521e77
kennedymwavu <mwavukennedy@gmail.com>
2023-05-27 14:30:40 +0300

    ft: show active sidebar item

--- www/sidebar.js
+++ www/sidebar.js
@@ -5,4 +5,19 @@
   tooltipTriggerList.forEach(tooltipTriggerEl => {
     new bootstrap.Tooltip(tooltipTriggerEl)
   })
-})()
+})();
+
+let sidebar_ul = document.querySelector("#sidebar_ul");
+let sidebar_li = sidebar_ul.querySelectorAll("li");
+sidebar_li.forEach((x) => {
+  x.addEventListener("click", () => {
+    // remove class 'active' from every other li:
+    let anchor_elements = sidebar_ul.querySelectorAll("a");
+    anchor_elements.forEach((a) => {
+      a.classList.remove("active");
+    });
+    
+    // add class 'active' to this element:
+    x.querySelector("a").classList.add("active");
+  });
+});
