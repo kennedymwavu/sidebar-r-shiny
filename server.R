f584043cbaea29690823b4b39b4cd36414acf006
kennedymwavu <mwavukennedy@gmail.com>
2023-05-27 23:18:36 +0300

    ft: add new tabpanel

--- server.R
+++ server.R
@@ -13,4 +13,51 @@ server <- function(input, output, session) {
       }
     )
   })
+
+  observeEvent(input$add_tab, {
+    id <- paste0("tab", as.numeric(Sys.time()))
+    # append the tab:
+    appendTab(
+      inputId = "tabs",
+      tab = tabPanelBody(
+        value = id,
+        tags$h1(paste("This is a dynamically inserted tab!", id))
+      )
+    )
+
+    # add the sidebar li:
+    insertUI(
+      selector = "#sidebar_ul li:last",
+      where = "afterEnd",
+      ui = tagList(
+        sidebar_li(
+          link_id = id,
+          icon_class = NULL,
+          label = id
+        )
+      )
+    )
+
+    # switch to the new tab:
+    freezeReactiveValue(input, "tabs")
+    updateTabsetPanel(
+      session = session,
+      inputId = "tabs",
+      selected = id
+    )
+
+    # switch to the new tab whenever the li element is clicked:
+    shinyjs::onclick(
+      id = id,
+      expr = {
+        # switch to the new tab:
+        freezeReactiveValue(input, "tabs")
+        updateTabsetPanel(
+          session = session,
+          inputId = "tabs",
+          selected = id
+        )
+      }
+    )
+  })
 }
